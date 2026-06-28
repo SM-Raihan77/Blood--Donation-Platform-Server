0e0cb5928d81517021d437e5f0a3f7268a16771e
SM-Raihan77 <smraihan720@gmail.com>
2026-06-28 22:55:57 +0600

    feat:add funding request

--- index.js
+++ index.js
@@ -36,6 +36,68 @@ async function run() {
         const fundingCollection = db.collection("fundings");
 
 
+        // ==========================
+        // POST FUNDING API
+        // ==========================
+
+        app.post("/api/funding", async (req, res) => {
+            //   console.log("Funding API called");
+            //   console.log(req.body);
+            try {
+                const { userEmail, amount, sessionId } = req.body;
+
+                if (!userEmail || !amount || !sessionId) {
+                    return res.status(400).json({
+                        success: false,
+                        message: "userEmail, amount and sessionId are required",
+                    });
+                }
+
+                const existing = await fundingCollection.findOne({ sessionId });
+
+                if (existing) {
+                    return res.status(409).json({
+                        success: false,
+                        message: "Funding already recorded",
+                    });
+                }
+
+                const result = await fundingCollection.insertOne({
+                    userEmail,
+                    amount: Number(amount),
+                    sessionId,
+                    createdAt: new Date(),
+                });
+
+                res.status(201).json({
+                    success: true,
+                    message: "Funding added successfully",
+                    insertedId: result.insertedId,
+                });
+            } catch (error) {
+                console.error("Error adding funding:", error);
+
+                res.status(500).json({
+                    success: false,
+                    message: "Failed to add funding",
+                });
+            }
+        });
+        // ==========================
+        // Transactions history API
+        // ==========================
+        app.get("/api/funding", async (req, res) => {
+            try {
+                const result = await fundingCollection
+                    .find()
+                    .sort({ createdAt: -1 })
+                    .toArray();
+
+                res.send(result);
+            } catch (error) {
+                res.status(500).send(error);
+            }
+        });
 
         // MongoDB deployment validation
         await client.db("admin").command({ ping: 1 });
